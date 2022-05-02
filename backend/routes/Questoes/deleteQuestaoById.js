aa1a985f81f271cb95f917341b29ccee5e908b75
Menta <gabrielmaxss@outlook.com>
2022-05-02 16:59:56 -0300

    varrendo coisas (banco e CRUD) do json-server

--- backend/routes/Questoes/deleteQuestaoById.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios({
-    method: 'delete',
-    url: `http://localhost:3000/questoes/:id`,
-})
-    .then((response) => console.log('deu certo. ' + response.status))
-    .catch((error) => console.error(error))
--- backend/routes/Usuarios/deleteUsuarioById.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios({
-    method: 'delete',
-    url: `http://localhost:3000/usuarios/:id`,
-})
-    .then((response) => console.log('deu certo. ' + response.status))
-    .catch((error) => console.error(error))
