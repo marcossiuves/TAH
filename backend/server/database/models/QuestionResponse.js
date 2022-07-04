3caefe3740197d03e19bff0cfc7ccd6185487489
Menta <gabrielmaxss@outlook.com>
2022-07-03 22:10:07 -0300

    avançando relacionamentos e rotas abertas

--- backend/server/database/models/QuestionResponse.js
+++ backend/server/database/models/QuestionResponse.js
@@ -1,19 +1,16 @@
 const { DataTypes } = require("sequelize");
 const database = require("../config");
-const Exam = require("./Exam");
-const Question = require("./Question");
+const QuestionExam = require("./QuestionExam");
 const User = require("./User");
 
 const QuestionResponse = database.define("QuestionResponse", {
   id_question_response: { type: DataTypes.INTEGER, autoIncrement: true,primaryKey: true},
-  id_exam: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
-  id_question: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+  id_question_exam: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
   option: { type: DataTypes.STRING, allowNull: false },
   id_user: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
 });
 
 QuestionResponse.belongsTo(User, { foreignKey: "id_user" });
-QuestionResponse.belongsTo(Exam, { foreignKey: "id_exam" });
-QuestionResponse.belongsTo(Question, { foreignKey: "id_question" });
+QuestionResponse.belongsTo(QuestionExam, { foreignKey: "id_question_exam" });
 
 module.exports = QuestionResponse;
